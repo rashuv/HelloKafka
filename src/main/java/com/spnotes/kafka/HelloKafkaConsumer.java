69e86cdd3a07b33a869da28b1ddccd7381b010bb
Mahesh Jadhav <mahesh430023@gmail.com>
2015-05-13 14:11:23 +0530

    minor refactoring changes

--- src/main/java/com/spnotes/kafka/HelloKafkaConsumer.java
+++ src/main/java/com/spnotes/kafka/HelloKafkaConsumer.java
@@ -1,31 +1,27 @@
 package com.spnotes.kafka;
 
-import kafka.api.FetchRequest;
-import kafka.api.FetchRequestBuilder;
-import kafka.api.FetchResponse;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.javaapi.consumer.SimpleConsumer;
-
-import java.nio.ByteBuffer;
-import java.util.*;
-
 import kafka.javaapi.message.ByteBufferMessageSet;
 import kafka.message.MessageAndOffset;
-import java.io.UnsupportedEncodingException;
-
 
 /**
  * Created by user on 8/4/14.
  */
-public class HelloKafkaConsumer extends  Thread {
+public class HelloKafkaConsumer extends Thread {
     final static String clientId = "SimpleConsumerDemoClient";
     final static String TOPIC = "testconsole";
     ConsumerConnector consumerConnector;
-
 
     public static void main(String[] argv) throws UnsupportedEncodingException {
         HelloKafkaConsumer helloKafkaConsumer = new HelloKafkaConsumer();
@@ -33,10 +29,10 @@ public class HelloKafkaConsumer extends  Thread {
 
     }
 
-    public HelloKafkaConsumer(){
+    public HelloKafkaConsumer() {
         Properties properties = new Properties();
-        properties.put("zookeeper.connect","localhost:2181");
-        properties.put("group.id","test-group");
+        properties.put("zookeeper.connect", "localhost:2181");
+        properties.put("group.id", "test-group");
         ConsumerConfig consumerConfig = new ConsumerConfig(properties);
         consumerConnector = Consumer.createJavaConsumerConnector(consumerConfig);
     }
@@ -45,16 +41,17 @@ public class HelloKafkaConsumer extends  Thread {
     public void run() {
         Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
         topicCountMap.put(TOPIC, new Integer(1));
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumerConnector.createMessageStreams(topicCountMap);
-        KafkaStream<byte[], byte[]> stream =  consumerMap.get(TOPIC).get(0);
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumerConnector
+                .createMessageStreams(topicCountMap);
+        KafkaStream<byte[], byte[]> stream = consumerMap.get(TOPIC).get(0);
         ConsumerIterator<byte[], byte[]> it = stream.iterator();
-        while(it.hasNext())
+        while (it.hasNext())
             System.out.println(new String(it.next().message()));
 
     }
 
     private static void printMessages(ByteBufferMessageSet messageSet) throws UnsupportedEncodingException {
-        for(MessageAndOffset messageAndOffset: messageSet) {
+        for (MessageAndOffset messageAndOffset : messageSet) {
             ByteBuffer payload = messageAndOffset.message().payload();
             byte[] bytes = new byte[payload.limit()];
             payload.get(bytes);
--- src/main/java/com/spnotes/kafka/HelloKafkaProducer.java
+++ src/main/java/com/spnotes/kafka/HelloKafkaProducer.java
@@ -1,13 +1,12 @@
 package com.spnotes.kafka;
 
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
-import kafka.producer.ProducerConfig;
-import scala.collection.Seq;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 
 /**
  * Created by user on 8/4/14.
@@ -15,15 +14,15 @@ import java.util.Properties;
 public class HelloKafkaProducer {
     final static String TOPIC = "testconsole";
 
-
-    public static void main(String[] argv){
+    public static void main(String[] argv) {
         Properties properties = new Properties();
-        properties.put("metadata.broker.list","localhost:9092");
-        properties.put("serializer.class","kafka.serializer.StringEncoder");
+        properties.put("metadata.broker.list", "localhost:9092");
+        properties.put("serializer.class", "kafka.serializer.StringEncoder");
         ProducerConfig producerConfig = new ProducerConfig(properties);
-        kafka.javaapi.producer.Producer<String,String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
+        Producer<String, String> producer = new Producer<String, String>(producerConfig);
         SimpleDateFormat sdf = new SimpleDateFormat();
-        KeyedMessage<String, String> message =new KeyedMessage<String, String>(TOPIC,"Test message from java program " + sdf.format(new Date()));
+        KeyedMessage<String, String> message = new KeyedMessage<String, String>(TOPIC,
+                "Test message from java program " + sdf.format(new Date()));
         producer.send(message);
         producer.close();
     }
